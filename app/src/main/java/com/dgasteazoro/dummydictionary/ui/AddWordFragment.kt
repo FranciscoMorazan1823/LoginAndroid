f4f759c58251772062f2aee140e105782a60a04c
dgasteazoro <00315919@uca.edu.sv>
2022-04-29 18:05:47 -0600

    added Add Word button

--- app/src/main/java/com/dgasteazoro/dummydictionary/ui/AddWordFragment.kt
+++ app/src/main/java/com/dgasteazoro/dummydictionary/ui/AddWordFragment.kt
@@ -10,10 +10,12 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.dgasteazoro.dummydictionary.DummyDictionaryApplication
 import com.dgasteazoro.dummydictionary.R
+import com.dgasteazoro.dummydictionary.data.model.Word
+import com.dgasteazoro.dummydictionary.databinding.FragmentAddWordBinding
 import com.dgasteazoro.dummydictionary.databinding.FragmentWordListBinding
 import com.dgasteazoro.dummydictionary.repository.DictionaryRepository
 
-class WordListFragment : Fragment() {
+class AddWordFragment : Fragment() {
     private val viewModelFactory by lazy {
         val application = requireActivity().application as DummyDictionaryApplication
         WordViewModelFactory(application.getDictionaryRepository())
@@ -21,33 +23,34 @@ class WordListFragment : Fragment() {
     private val viewModel: WordViewModel by viewModels {
         viewModelFactory
     }
-    private lateinit var binding: FragmentWordListBinding
+    private lateinit var binding: FragmentAddWordBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_word_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_word, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val wordListRecyclerView = binding.wordListRecyclerView
-        val wordAdapter = WordAdapter()
-        wordListRecyclerView.apply {
-            adapter = wordAdapter
-        }
-        viewModel.words.observe(viewLifecycleOwner) { data ->
-            wordAdapter.setData(data)
-        }
-        binding.lifecycleOwner = viewLifecycleOwner
         val navController = findNavController()
-        binding.actionAddWord.setOnClickListener {
-            val action = WordListFragmentDirections.actionWordListFragmentToAddWordFragment()
+        binding.addWordButton.setOnClickListener {
+            viewModel.addWord(
+                Word(
+                    binding.wordText.text.toString(),
+                    binding.definitionText.text.toString(),
+                    false
+                )
+            )
+            val action = AddWordFragmentDirections.actionAddWordFragmentToWordListFragment()
             navController.navigate(action)
         }
+
+
     }
 }
+
